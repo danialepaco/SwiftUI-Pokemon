59670d76d6b07a0b397ac23232ed1cf2f653cac9
daniel parra <danialepaco@hotmail.com>
2020-04-29 18:29:50 -0500

    Added red rectangle

--- pokemon/ContentView.swift
+++ pokemon/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Rectangle()
+            .fill(Color.red)
+            .cornerRadius(15)
+            .frame(width: 150, height: 150)
     }
 }
 
