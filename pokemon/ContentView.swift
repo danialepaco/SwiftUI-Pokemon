e43aac08c552bc3af89101214124e06046fa5e0a
daniel parra <danialepaco@hotmail.com>
2020-04-29 18:24:27 -0500

    upload project

---
+++ pokemon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pokemon
+//
+//  Created by daniel parra on 29/04/20.
+//  Copyright © 2020 daniel parra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
